d3a1ad04956dfc31849ba7c236f2cd4b8fdd93be
Mert Akpınar <mert.akpinar@hotmail.com>
2022-09-06 14:15:12 +0300

    comments

--- component.js
+++ component.js
@@ -1,18 +1,26 @@
-"use strict";
-
+// createElement method of React creates a React component
 const loginForm = React.createElement;
 
+// Creating a class Component by extending React.component class
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  // render method takes what will be rendered by LoginForm class component.
   render() {
     return loginForm(
+      // createElement() method takes element name as the first parameter.
       "form",
+      // createElement() method takes props for that element as the second parameter.
       {
+        // Declaring onSubmit event handler to define what is going to happen when form is submitted.
         onSubmit: (e) => {
+          /* preventDefault() method prevents the default action of form submission which 
+             refreshes the page.
+          */
           e.preventDefault();
+          /* Getting values from input fields and console logging them */
           const user = {
             username: document.querySelector("#username").value,
             password: document.querySelector("#password").value,
@@ -21,10 +29,15 @@ class LoginForm extends React.Component {
           /* should have make a request to login route with these creds in a real world application */
         },
       },
+      // createElement() method takes child elements as the third parameter.
       [
+        // First child is HTML div element that has input element inside of it.
         React.createElement(
+          // element name
           "div",
+          // props
           { className: "input-field" },
+          // child element
           React.createElement("input", {
             type: "username",
             name: "username",
@@ -33,6 +46,7 @@ class LoginForm extends React.Component {
             style: { marginBottom: 10 },
           })
         ),
+        // password input element as 2nd child of form element
         React.createElement(
           "div",
           { className: "input-field", style: { marginBottom: 10 } },
@@ -43,12 +57,16 @@ class LoginForm extends React.Component {
             placeholder: "Enter your password",
           })
         ),
+        // a button to submit the form as 3rd child element of the form.
         React.createElement("button", { type: "submit" }, "Submit"),
       ]
     );
   }
 }
 
+// grabbing an element from index.html that has "react-component" as its id.
 const domContainer = document.querySelector("#react-component");
+// root is where our components will be rendered
 const root = ReactDOM.createRoot(domContainer);
+// render LoginForm component in a div that has "react-component" as its id
 root.render(loginForm(LoginForm));
